58fd7b5564f252adb51d89dae5a3a1a77d2ac198
PVerras <57714783+panosverras@users.noreply.github.com>
2025-09-20 12:48:06 +0300

    Update wro-basic.ts

--- wro-basic.ts
+++ wro-basic.ts
@@ -12,6 +12,23 @@
  */
 //% weight=100 color=#edca1a icon="\uf0e7"
 namespace PlanetX_WRO {
+
+    export enum TrackbitType {
+        //% block="◌" 
+        State_0 = 0,
+        //% block="●" 
+        State_1 = 1
+    }
+    export enum TrackbitChannel {
+        //% block="1"
+        One = 0,
+        //% block="2"
+        Two = 1,
+        //% block="3"
+        Three = 2,
+        //% block="4"
+        Four = 3
+    }
     
 
     /**
@@ -29,6 +46,32 @@ namespace PlanetX_WRO {
         offset = (offsetH << 8) | offsetL
         offset = Math.map(offset, 0, 6000, -3000, 3000)
         return offset;
+    }
+
+
+    //% state.fieldEditor="gridpicker" state.fieldOptions.columns=2
+    //% channel.fieldEditor="gridpicker" channel.fieldOptions.columns=4
+    //% subcategory=Sensor group="IIC Port"
+    //% block="Trackbit channel %channel is %state"
+    export function TrackbitChannelState(channel: TrackbitChannel, state: TrackbitType): boolean {
+        let TempVal: number = 0
+        pins.i2cWriteNumber(0x1a, 4, NumberFormat.Int8LE)
+        TempVal = pins.i2cReadNumber(0x1a, NumberFormat.UInt8LE, false)
+        if (state == TrackbitType.State_1)
+            if (TempVal & 1 << channel) {
+                return true
+            }
+            else {
+                return false
+            }
+        else {
+            if (TempVal & 1 << channel) {
+                return false
+            }
+            else {
+                return true
+            }
+        }
     }
 
 
