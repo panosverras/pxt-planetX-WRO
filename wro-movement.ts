3387f34980943f88e9b9d444a1de660b65f1183f
PVerras <57714783+panosverras@users.noreply.github.com>
2025-09-20 21:54:27 +0300

    Update wro-movement.ts

--- wro-movement.ts
+++ wro-movement.ts
@@ -440,9 +440,9 @@ namespace nezhaV2_WRO {
 
     //% group="LineFollow functions"
     //% weight=350
-    //% block="TrackBit PD LineFollow\r\n MotorLeft %_motorleft MotorRight %_motorright BaseSpeed %_basespeed Kp %_kp Kd %_kd LastError %_pidPreviousError"
+    //% block="TrackBit PD LineFollow        MotorLeft %_motorleft MotorRight %_motorright BaseSpeed %_basespeed Kp %_kp Kd %_kd LastError %_pidPreviousError"
     //% _basespeed.min=0  _basespeed.max=100
-    export function pd_linefollow(_pidPreviousError: number, _kp: number, _kd: number, _basespeed: number, _motorleft: MotorPostion, _motorright: MotorPostion): number {
+    export function pd_linefollow(_motorleft: MotorPostion, _motorright: MotorPostion, _basespeed: number, _kp: number, _kd: number, _pidPreviousError: number): number {
         
         let _pidError = _trackbit_get_offset()
         let _pidValue = (_kp * _pidError) + (_kd * (_pidError - _pidPreviousError))
